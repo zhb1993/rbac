f8482a3ddd45f977a447a74d5786f664f738a48e
zhanghongbin <976109421@qq.com>
2020-06-17 13:37:12 +0800

    初始化

---
+++ src/test/java/com/zhb/rbac/RbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhb.rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
