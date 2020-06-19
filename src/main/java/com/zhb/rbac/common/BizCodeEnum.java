dfec43d03521370a68368831cc8c15a4d5aa9400
zhb1993 <976109421@qq.com>
2020-06-20 02:15:51 +0800

    提交基础代码

---
+++ src/main/java/com/zhb/rbac/common/BizCodeEnum.java
@@ -0,0 +1,24 @@
+package com.zhb.rbac.common;
+
+
+public enum BizCodeEnum {
+
+    /**
+     * 默认业务代码值
+     */
+    DEFAULT(20000),
+    MINI_APP_LOGIN(20001); // 小程序授权成功，返回代码值，去绑定手机号码
+    private int code;
+
+    BizCodeEnum(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/com/zhb/rbac/common/CodeEnum.java
@@ -0,0 +1,52 @@
+package com.zhb.rbac.common;
+
+/**
+ * @author jackesy
+ * @className: CodeEnum
+ * @description: 响应代码枚举类
+ * @create 2019/8/14 14:26
+ **/
+public enum CodeEnum {
+
+
+    SUCCESS(100, "操作成功"),
+    FAIL_PARAMCHECK(101,"参数校验失败"),
+    FAIL_BUSINESS(102,"操作失败"),
+    FAIL_LOGIN_OVERDUE(401,"登录过期"),
+    FAIL_NOT_LOGIN(402,"没有登录"),
+    FAIL_ILLEGAL_TOKEN(403,"非法token"),
+    FAIL_NOTFOUND(404,"请求接口不存在"),
+    FAIL_NOAUTHORITY(405,"无权限请求接口"),
+    FAIL_RATELIMIT(429,"访问太频繁"),
+    FAIL_SERVER(500,"程序内部异常"),
+    ;
+
+
+    private int code;
+    private String msg;
+
+    CodeEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static Boolean isSuccess(int code) {
+        return SUCCESS.getCode() == code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
